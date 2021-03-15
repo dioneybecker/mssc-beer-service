29378d68c465485a092aec96f862b6c4c7777d84
Dioney Becker <Dioney.Becker@sprint.com>
2021-03-15 09:29:30 -0300

    refactor to use config-repo
    Change-Id: I1310001ebc39c5f22c766bdffbcedf9da412722d

--- src/main/java/com/dioneybecker/msscbeerservice/config/ServiceDiscovery.java
+++ src/main/java/com/dioneybecker/msscbeerservice/config/ServiceDiscovery.java
@@ -3,9 +3,33 @@ package com.dioneybecker.msscbeerservice.config;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
+import com.netflix.appinfo.AmazonInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @EnableDiscoveryClient
 @Configuration
 public class ServiceDiscovery {
+
+    @Bean
+    @Profile("!default")
+    public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+
+        log.info("Public Hostname: " +  info.get(AmazonInfo.MetaDataKey.publicHostname));
+        log.info("Public IPv4: " + info.get(AmazonInfo.MetaDataKey.publicIpv4));
+
+        config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+        config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+        config.setDataCenterInfo(info);
+        return config;      
+    }
     
 }
