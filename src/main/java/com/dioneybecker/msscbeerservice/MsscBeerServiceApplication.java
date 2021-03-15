df4fe5e00373d93d214ae56c573212346dd7ec24
Dioney Becker <Dioney.Becker@sprint.com>
2021-03-15 20:29:07 -0300

    removed(exclude ContextRegionProviderAutoConfiguration
    Change-Id: I5471ffe505d48f34754b0fb128fae624117e44ab

--- src/main/java/com/dioneybecker/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/com/dioneybecker/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,11 +2,10 @@ package com.dioneybecker.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication(exclude = ContextRegionProviderAutoConfiguration.class)
+@SpringBootApplication
 public class MsscBeerServiceApplication {
 
 	public static void main(String[] args) {
